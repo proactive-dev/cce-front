c5b57b77b647ed831a294963608c9f49519b03fe
Apollo IT <p987415@gmail.com>
2019-12-06 15:46:07 +0000

    feat: Add API

--- src/api/axiosAPIs.js
+++ src/api/axiosAPIs.js
@@ -52,15 +52,207 @@ export const verifyTFA = (data) => {
 }
 
 export const forgotPassword = (data) => {
-  return axiosRequest('post', axiosConfig.FORGOT_PWD_URL, data)
+  return axiosRequest('post', axiosConfig.RESET_PWD_URL, data)
+}
+
+export const validatePwdToken = (token) => {
+  return axiosRequest('get', `${axiosConfig.RESET_PWD_URL}/${token}`)
+}
+
+export const resetPassword = (token, data) => {
+  return axiosRequest('put', `${axiosConfig.RESET_PWD_URL}/${token}`, data)
+}
+
+export const changePassword = (data) => {
+  return axiosRequest('post', axiosConfig.CHANGE_PWD_URL, data)
 }
 
 export const getAuthStatus = (needLoader = false) => {
   return axiosRequest('get', axiosConfig.AUTH_STATUS_URL, null, needLoader)
 }
 
+export const postTfa = (data) => {
+  return axiosRequest('put', axiosConfig.TFA_URL, data)
+}
+
+export const getCurrencies = (needLoader = false) => {
+  return axiosRequest('get', axiosConfig.CURRENCIES_URL, null, needLoader)
+}
+
+export const getLevels = (needLoader = false) => {
+  return axiosRequest('get', axiosConfig.LEVELS_URL, null, needLoader)
+}
+
+export const getMarketConfigs = (needLoader = false) => {
+  return axiosRequest('get', axiosConfig.MARKET_CONFIGS_URL, null, needLoader)
+}
+
+export const getMarkets = (needLoader = false) => {
+  return axiosRequest('get', axiosConfig.MARKETS_URL, null, needLoader)
+}
+
+export const getTicker = (market, needLoader = false) => {
+  return axiosRequest('get', `${axiosConfig.TICKERS_URL}/${market}`, null, needLoader)
+}
+
+export const getOHLC = (market, period = 60, limit = 768, needLoader = false) => {
+  return axiosRequest('get', `${axiosConfig.OHLC_URL}?market=${market}&period=${period}&limit=${limit}`, null, needLoader)
+}
+
+export const newOrderBid = (market, data) => {
+  return axiosRequest('post', `${axiosConfig.MARKETS_URL}/${market}/order_bids`, data)
+}
+
+export const newOrderAsk = (market, data) => {
+  return axiosRequest('post', `${axiosConfig.MARKETS_URL}/${market}/order_asks`, data)
+}
+
+export const clearOrderBids = (market, data) => {
+  return axiosRequest('post', `${axiosConfig.MARKETS_URL}/${market}/order_bids/clear`, data)
+}
+
+export const clearOrderAsks = (market, data) => {
+  return axiosRequest('post', `${axiosConfig.MARKETS_URL}/${market}/order_asks/clear`, data)
+}
+
+export const clearOrders = (market) => {
+  return axiosRequest('post', `${axiosConfig.MARKETS_URL}/${market}/orders/clear`)
+}
+
+export const clearOrder = (market, order) => {
+  return axiosRequest('delete', `${axiosConfig.MARKETS_URL}/${market}/orders/${order}`)
+}
+
+export const getAllAccounts = (needLoader = false) => {
+  return axiosRequest('get', axiosConfig.ALL_ACCOUNTS_URL, null, needLoader)
+}
+
 export const getAccounts = (needLoader = false) => {
   return axiosRequest('get', axiosConfig.ACCOUNTS_URL, null, needLoader)
+}
+
+export const getMainAccounts = (needLoader = false) => {
+  return axiosRequest('get', axiosConfig.MAIN_ACCOUNTS_URL, null, needLoader)
+}
+
+export const getMember = (needLoader = false) => {
+  return axiosRequest('get', axiosConfig.MEMBER_URL, null, needLoader)
+}
+
+export const updateMember = (data) => {
+  return axiosRequest('post', axiosConfig.MEMBER_URL, data)
+}
+
+export const getAllOrderHistory = () => {
+  return axiosRequest('get', axiosConfig.HISTORY_ORDER_URL)
+}
+
+export const getAllTradeHistory = () => {
+  return axiosRequest('get', axiosConfig.HISTORY_TRADE_URL)
+}
+
+export const getOrderHistory = (params) => {
+  return axiosRequest('get', `${axiosConfig.HISTORY_ORDER_URL}?page=${params.page}&perPage=${params.perPage}&search=${params.search}`)
+}
+
+export const getTradeHistory = (params) => {
+  return axiosRequest('get', `${axiosConfig.HISTORY_TRADE_URL}?page=${params.page}&perPage=${params.perPage}&search=${params.search}`)
+}
+
+export const getAddresses = () => {
+  return axiosRequest('get', axiosConfig.ADDR_MNG_URL)
+}
+
+export const newAddress = (data) => {
+  return axiosRequest('post', axiosConfig.ADDR_MNG_URL, data)
+}
+
+export const deleteAddress = (id) => {
+  return axiosRequest('delete', `${axiosConfig.ADDR_MNG_URL}/${id}`)
+}
+
+export const getDeposits = (currency) => {
+  if (!!currency) {
+    return axiosRequest('get', `${axiosConfig.DEPOSITS_URL}?currency=${currency}`)
+  } else {
+    return axiosRequest('get', axiosConfig.DEPOSITS_URL)
+  }
+}
+
+export const getWithdraws = (currency) => {
+  if (!!currency) {
+    return axiosRequest('get', `${axiosConfig.WITHDRAWS_URL}?currency=${currency}`)
+  } else {
+    return axiosRequest('get', axiosConfig.WITHDRAWS_URL)
+  }
+}
+
+export const newWithdraw = (currency, data) => {
+  return axiosRequest('post', `${axiosConfig.WITHDRAW_URL}/${currency}`, data)
+}
+
+export const cancelWithdraw = (currency, id) => {
+  return axiosRequest('delete', `${axiosConfig.WITHDRAW_URL}/${currency}/${id}`)
+}
+
+export const getTickets = (params) => {
+  return axiosRequest('get', `${axiosConfig.TICKETS_URL}?page=${params.page}&perPage=${params.perPage}`)
+}
+
+export const newTicket = (data) => {
+  return axiosRequest('post', axiosConfig.TICKETS_URL, data)
+}
+
+export const getTicket = (id) => {
+  return axiosRequest('get', `${axiosConfig.TICKETS_URL}/${id}`)
+}
+
+export const closeTicket = (id) => {
+  return axiosRequest('delete', `${axiosConfig.TICKETS_URL}/${id}`)
+}
+
+export const newTicketComment = (id, data) => {
+  return axiosRequest('post', `${axiosConfig.TICKETS_URL}/${id}/comments`, data)
+}
+
+export const activate = () => {
+  return axiosRequest('get', axiosConfig.ACTIVATION_URL)
+}
+
+export const updateIdDocument = (data) => {
+  return axiosRequest('put', axiosConfig.ID_DOCUMENT_URL, data)
+}
+
+export const getGoogleAuth = () => {
+  return axiosRequest('get', axiosConfig.GOOGLE_AUTH_URL)
+}
+
+export const enableGoogleAuth = (data) => {
+  return axiosRequest('put', axiosConfig.GOOGLE_AUTH_URL, data)
+}
+
+export const disableGoogleAuth = (data) => {
+  return axiosRequest('post', axiosConfig.GOOGLE_AUTH_URL, data)
+}
+
+export const getApiTokens = () => {
+  return axiosRequest('get', axiosConfig.API_TOKENS_URL)
+}
+
+export const newApiToken = (data) => {
+  return axiosRequest('post', axiosConfig.API_TOKENS_URL, data)
+}
+
+export const updateApiToken = (id, data) => {
+  return axiosRequest('put', `${axiosConfig.API_TOKENS_URL}/${id}`, data)
+}
+
+export const deleteApiToken = (id) => {
+  return axiosRequest('delete', `${axiosConfig.API_TOKENS_URL}/${id}`)
+}
+
+export const moveFunds = (data) => {
+  return axiosRequest('post', axiosConfig.MOVE_FUNDS_URL, data)
 }
 
 export const getRefData = (data = null, needLoader = false) => {
--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -6,15 +6,35 @@ import { PRODUCTION_URL } from '../constants/AppConfigs'
 
 export const HOST_URL = process.env.NODE_ENV === 'production' ? `${PRODUCTION_URL}:8443` : 'http://localhost:4000'
 
+export const HOST_API_URL = `${HOST_URL}/api/v2`
 export const REGISTER_URL = `${HOST_URL}/auth/identity/register`
 export const LOGIN_URL = `${HOST_URL}/auth/identity/callback`
 export const LOGOUT_URL = `${HOST_URL}/signout`
 export const TFA_VERIFY_URL = `${HOST_URL}/auth/app`
-export const FORGOT_PWD_URL = `${HOST_URL}/reset_passwords`
+export const RESET_PWD_URL = `${HOST_URL}/reset_passwords`
 export const AUTH_STATUS_URL = `${HOST_URL}/auth/status`
-export const ACCOUNTS_URL = `${HOST_URL}/accounts/main?simple=true`
+export const MEMBER_URL = `${HOST_URL}/member`
+export const CHANGE_PWD_URL = `${HOST_URL}/identity`
+export const ACTIVATION_URL = `${HOST_URL}/activations/new`
+export const GOOGLE_AUTH_URL = `${HOST_URL}/verify/google_auth`
+export const API_TOKENS_URL = `${HOST_URL}/api_tokens`
+export const ID_DOCUMENT_URL = `${HOST_URL}/id_document`
+export const TFA_URL = `${HOST_URL}/two_factors/app`
+export const MARKETS_URL = `${HOST_URL}/markets`
+export const TICKERS_URL = `${HOST_API_URL}/tickers`
+export const OHLC_URL = `${HOST_API_URL}/k`
 export const REF_DATA_URL = `${HOST_URL}/referrals?simple=true`
-
-// export const MARKETS_URL = `${HOST_URL}/markets`
-// export const PURCHASE_CONFIG_URL = `${HOST_URL}/configs/purchase`
-// export const TWO_FACTOR_URL = `${HOST_URL}/two_factors/app`
+export const ALL_ACCOUNTS_URL = `${HOST_URL}/accounts`
+export const ACCOUNTS_URL = `${HOST_URL}/accounts/main?simple=true`
+export const MAIN_ACCOUNTS_URL = `${HOST_URL}/accounts/main`
+export const CURRENCIES_URL = `${HOST_URL}/configs/currencies`
+export const LEVELS_URL = `${HOST_URL}/configs/levels`
+export const MARKET_CONFIGS_URL = `${HOST_URL}/configs/markets`
+export const HISTORY_ORDER_URL = `${HOST_URL}/history/orders`
+export const HISTORY_TRADE_URL = `${HOST_URL}/history/trades`
+export const ADDR_MNG_URL = `${HOST_URL}/fund_sources`
+export const WITHDRAWS_URL = `${HOST_URL}/funds/withdraws`
+export const DEPOSITS_URL = `${HOST_URL}/funds/deposits`
+export const WITHDRAW_URL = `${HOST_URL}/withdraws`
+export const MOVE_FUNDS_URL = `${HOST_URL}/move_funds`
+export const TICKETS_URL = `${HOST_URL}/tickets`
