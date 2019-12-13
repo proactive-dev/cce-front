b3bfa32452764577b7900249c4a9bb0cef7dc62c
Apollo IT <p987415@gmail.com>
2019-12-13 10:53:33 +0000

    feat: Add trading rule page

--- src/routes/common/TradingRules.js
+++ src/routes/common/TradingRules.js
@@ -1,14 +1,52 @@
 import React from 'react'
 import { FormattedMessage, injectIntl } from 'react-intl'
+import { Table } from 'antd'
+import { getFixed, getTableLocaleData } from '../../util/helpers'
+import { MARKETS } from '../../constants/Markets'
 
 class TradingRules extends React.Component {
-  render() {
-    const {intl} = this.props
 
+  getColumns() {
+    const {intl} = this.props
+    return [
+      {
+        title: intl.formatMessage({id: 'pair'}),
+        dataIndex: 'name',
+        align: 'center',
+        render: (value) => {
+          return <span className={'gx-font-weight-bold'}>{value}</span>
+        }
+      },
+      {
+        title: intl.formatMessage({id: 'ask.min'}),
+        dataIndex: 'ask',
+        align: 'center',
+        render: (value) => {
+          return <span>{getFixed(value.min, value.fixed)}&nbsp;{value.currency.toUpperCase()}</span>
+        }
+      },
+      {
+        title: intl.formatMessage({id: 'bid.min'}),
+        dataIndex: 'bid',
+        align: 'center',
+        render: (value) => {
+          return <span>{getFixed(value.min, value.fixed)}&nbsp;{value.currency.toUpperCase()}</span>
+        }
+      }
+    ]
+  }
+
+  render() {
     return (
       <div>
         <h1 className="gx-mt-4 gx-mb-4"><FormattedMessage id="trading.rules"/></h1>
-        {/* Components */}
+        <Table className={'gx-table-responsive gx-mb-4'}
+               columns={this.getColumns()}
+               dataSource={MARKETS}
+               pagination={false}
+               locale={getTableLocaleData}
+               rowKey="id"
+               size='medium'/>
       </div>
     )
   }
