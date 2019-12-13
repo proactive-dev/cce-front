15ddc8008365022fe021241cd3a42ecc5b113bdc
yoshimura <wagdev1919@gmail.com>
2019-12-13 19:16:01 +0800

    added resource

--- src/routes/main/NewApiToken.js
+++ src/routes/main/NewApiToken.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { FormattedMessage, injectIntl } from 'react-intl'
-import { Card, Form, Input, Spin, Typography } from 'antd'
+import { Button, Card, Col, Form, Input, Row, Spin, Typography } from 'antd'
 import { getAuthStatus } from '../../appRedux/actions/User'
+import { newApiToken } from '../../api/axiosAPIs'
+import { IconNotification } from '../../components/IconNotification'
+import { SUCCESS } from '../../constants/AppConfigs'
 
-const {Text} = Typography
+const {Text, Title} = Typography
 
 class NewApiToken extends React.Component {
   constructor(props) {
@@ -27,24 +30,77 @@ class NewApiToken extends React.Component {
     this.props.getAuthStatus()
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        let formData = new FormData()
+        formData.append('api_token[label]', values.label)
+        formData.append('two_factor[type]', 'app')
+        formData.append('two_factor[otp]', values.twoFactor)
+        formData.append('commit', 'Confirm')
+        let that = this
+        newApiToken(formData)
+          .then(response => {
+            const token = response.data
+            IconNotification(SUCCESS, this.props.intl.formatMessage({id: 'new.token.success'}))
+          })
+      }
+    })
+  }
+
   render() {
     const {intl} = this.props
     const {loader} = this.state
-
+    const {getFieldDecorator} = this.props.form
     return (
       <div>
         <h1 className="gx-mt-4 gx-mb-4"><FormattedMessage id="api.tokens"/></h1>
         <Spin spinning={loader} size="large">
           {/* Components */}
         </Spin>
-        <Card bordered={false}>
-          <Text><FormattedMessage id="api.tokens.create.description"/></Text>
-          <Form>
-            <Form.Item>
-              <Input/>
-            </Form.Item>
-          </Form>
-        </Card>
+        <Row type='flex' justify='center'>
+          <Col>
+            <Card bordered={false} style={{maxWidth: 600}}>
+              <Row type='flex' justify='center'>
+                <Col>
+                  <img alt="" src={require('assets/images/api.png')}/>
+                </Col>
+              </Row>
+              <Row type='flex' justify='center' className='gx-mt-2'>
+                <Col>
+                  <Text className={'gx-fs-lg'}>
+                    <FormattedMessage id="api.tokens.create.description"/>
+                  </Text>
+                </Col>
+              </Row>
+              <Form onSubmit={this.handleSubmit}>
+                <Form.Item label={intl.formatMessage({id: 'label'})} className={'gx-mt-2'} wrapperCol={{sm: 24}}
+                           style={{width: '100%', margin: 0}}>
+                  {getFieldDecorator('label', {
+                    rules: [{required: true, message: intl.formatMessage({id: 'alert.fieldRequired'})}]
+                  })(
+                    <Input/>)}
+                </Form.Item>
+
+                <Form.Item label={intl.formatMessage({id: 'google.auth.code'})} className={'gx-mt-2'}
+                           wrapperCol={{sm: 24}} style={{width: '100%', margin: 0}}>
+                  {getFieldDecorator('twoFactor', {
+                    rules: [{required: true, message: intl.formatMessage({id: 'alert.fieldRequired'})}]
+                  })(
+                    <Input/>)}
+                </Form.Item>
+                <Row type='flex' justify='center' className='gx-mt-2'>
+                  <Col>
+                    <Button type="primary" htmlType="submit">
+                      <FormattedMessage id="submit"/>
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
       </div>
     )
   }
@@ -60,4 +116,6 @@ const mapStateToProps = ({progress}) => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(NewApiToken))
+const WrappedNewApiTokenForm = Form.create()(NewApiToken)
+
+export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(WrappedNewApiTokenForm))
