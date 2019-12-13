36ca5a7bf2d6a60b57dac67df251730d73821fe6
YR199111RY <YR199111RY@protonmail.com>
2019-12-13 17:09:27 +0900

    refactor: Add OpenOrdersTable

---
+++ src/components/OpenOrdersTable.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import { Table } from 'antd'
+import { FormattedMessage, injectIntl } from 'react-intl'
+import { getFixed, getTableLocaleData, getTimeForTable } from '../util/helpers'
+
+class OpenOrdersTable extends React.Component {
+
+  handleTableChange = (pagination, filters, sorter) => {
+    this.props.onChange(pagination, filters, sorter)
+  }
+
+  getColumns() {
+    const {intl} = this.props
+    return [
+      {
+        title: intl.formatMessage({id: 'date'}),
+        dataIndex: 'at',
+        align: 'center',
+        render: (value) => {
+          return getTimeForTable(value)
+        }
+      },
+      {
+        title: intl.formatMessage({id: 'pair'}),
+        dataIndex: 'marketName',
+        align: 'center'
+      },
+      {
+        title: intl.formatMessage({id: 'side'}),
+        dataIndex: 'kind',
+        align: 'center',
+        render: (value) => {
+          return <FormattedMessage id={value}/>
+        }
+      },
+      {
+        title: intl.formatMessage({id: 'price'}),
+        dataIndex: 'price',
+        align: 'center',
+        render: (value, record) => {
+          return getFixed(value, record.priceFixed)
+        }
+      },
+      {
+        title: intl.formatMessage({id: 'amount'}),
+        dataIndex: 'origin_volume',
+        align: 'center',
+        render: (value, record) => {
+          return getFixed(value, record.amountFixed)
+        }
+      },
+      {
+        title: intl.formatMessage({id: 'filled%'}),
+        dataIndex: 'filled',
+        align: 'center',
+        render: (value, record) => {
+          return getFixed((record.origin_volume - record.volume) * 100 / record.origin_volume, 2)
+        }
+      },
+      {
+        title: intl.formatMessage({id: 'total'}),
+        dataIndex: 'total',
+        align: 'center',
+        render: (value, record) => {
+          return getFixed(record.origin_volume * record.price, record.priceFixed)
+        }
+      }
+    ]
+  }
+
+  render() {
+    const {dataList, intl, pagination} = this.props
+
+    return (
+      <div>
+        <Table className="gx-table-responsive gx-mt-4 gx-mb-4"
+               columns={this.getColumns()}
+               dataSource={dataList}
+               pagination={pagination}
+               locale={getTableLocaleData(intl)}
+               onChange={this.handleTableChange}
+               rowKey={'id'}
+               size='middle'/>
+      </div>
+    )
+  }
+}
+
+export default injectIntl(OpenOrdersTable)
--- src/routes/main/OpenOrders.js
+++ src/routes/main/OpenOrders.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { FormattedMessage, injectIntl } from 'react-intl'
 import { getAuthStatus } from '../../appRedux/actions/User'
-import { Spin, Table } from 'antd'
-import { getFixed, getTableLocaleData, getTimeForTable } from '../../util/helpers'
+import { Spin } from 'antd'
 import { getOrderHistory } from '../../api/axiosAPIs'
 import { MARKETS } from '../../constants/Markets'
+import OpenOrdersTable from '../../components/OpenOrdersTable'
 
 class OpenOrders extends React.Component {
   constructor(props) {
@@ -65,81 +65,18 @@ class OpenOrders extends React.Component {
     this.fetchData({page: pagination.current})
   }
 
-  getColumns() {
-    const {intl} = this.props
-    return [
-      {
-        title: intl.formatMessage({id: 'date'}),
-        dataIndex: 'at',
-        align: 'center',
-        render: (value) => {
-          return getTimeForTable(value)
-        }
-      },
-      {
-        title: intl.formatMessage({id: 'pair'}),
-        dataIndex: 'marketName',
-        align: 'center'
-      },
-      {
-        title: intl.formatMessage({id: 'side'}),
-        dataIndex: 'kind',
-        align: 'center',
-        render: (value) => {
-          return <FormattedMessage id={value}/>
-        }
-      },
-      {
-        title: intl.formatMessage({id: 'price'}),
-        dataIndex: 'price',
-        align: 'center',
-        render: (value, record) => {
-          return getFixed(value, record.priceFixed)
-        }
-      },
-      {
-        title: intl.formatMessage({id: 'amount'}),
-        dataIndex: 'origin_volume',
-        align: 'center',
-        render: (value, record) => {
-          return getFixed(value, record.amountFixed)
-        }
-      },
-      {
-        title: intl.formatMessage({id: 'filled%'}),
-        dataIndex: 'filled',
-        align: 'center',
-        render: (value, record) => {
-          return getFixed((record.origin_volume - record.volume) * 100 / record.origin_volume, 2)
-        }
-      },
-      {
-        title: intl.formatMessage({id: 'total'}),
-        dataIndex: 'total',
-        align: 'center',
-        render: (value, record) => {
-          return getFixed(record.origin_volume * record.price, record.priceFixed)
-        }
-      }
-    ]
-  }
-
   render() {
-    const {intl} = this.props
     const {loader, orders, pagination} = this.state
 
     return (
       <div>
         <h1 className="gx-mt-4 gx-mb-4"><FormattedMessage id="open.orders"/></h1>
         <Spin spinning={loader} size="large">
-          <Table className="gx-table-responsive gx-mt-4 gx-mb-4"
-                 columns={this.getColumns()}
-                 dataSource={orders}
-                 pagination={pagination}
-                 locale={getTableLocaleData(intl)}
-                 onChange={this.onChangeTable}
-                 rowKey={'id'}
-                 size='middle'/>
+          <OpenOrdersTable
+            pagination={pagination}
+            dataList={orders}
+            onChange={this.onChangeTable}
+          />
         </Spin>
       </div>
     )
